8f9417d15a09b071e2e5a89a8a07e03567388269
Ferhat <ferhatkarakus.dev@gmail.com>
2023-10-03 00:57:33 +0300

    commit2

---
+++ src/components/UI/Offcanvas.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Offcanvas.css";
+
+const Offcanvas = () => {
+  return (
+    <div>
+      <div className="backdrop"></div>
+    </div>
+  );
+};
+
+export default Offcanvas;
